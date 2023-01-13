7028d7b231358944708ebaa888d4827fe477d236
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-01-13 09:55:14 +0000

    build: create project with Spring Initializer

---
+++ src/main/kotlin/study/following/tutorial/youtube/thenewboston/ThenewbostonApplication.kt
@@ -0,0 +1,11 @@
+package study.following.tutorial.youtube.thenewboston
+
+import org.springframework.boot.runApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TheNewBostonApplication
+
+fun main(args: Array<String>) {
+	runApplication<TheNewBostonApplication>(*args)
+}
---
+++ src/test/kotlin/study/following/tutorial/youtube/thenewboston/ThenewbostonApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.following.tutorial.youtube.thenewboston
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheNewBostonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
